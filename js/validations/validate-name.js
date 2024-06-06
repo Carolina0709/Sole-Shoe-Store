c9c53ead8412fd751aef11978b31810cc1d9014e
Carolina0709 <carolina.gmartin@alumnos.udg.mx>
2024-06-05 18:28:49 -0600

    Name regex solved

--- js/validations/validate-name.js
+++ js/validations/validate-name.js
@@ -1,11 +1,11 @@
 export default function isValidName(input) {
-    // Expresión regular que verifica si el nombre contiene solo letras
-    const nameRegex = /^[a-zA-Z]+$/;
+    // Expresión regular que verifica si el nombre contiene solo letras y espacios
+    const nameRegex = /^[a-zA-Z\s]+$/;
 
-    // Verificar si el nombre cumple con el patrón de solo letras
+    // Verificar si el nombre cumple con el patrón de letras y espacios
     if (nameRegex.test(input.value)) {
         console.log("True");
     } else {
-        input.setCustomValidity("El nombre solo puede contener letras. ");
+        input.setCustomValidity("El nombre solo puede contener letras y espacios. ");
     }
 }
